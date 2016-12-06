b2d111f1bf8793909bdccea8743530f74c8f2cd9
Daniel Sauve <djsauve@rogers.com>
2016-12-06 12:10:41 -0500

    part 2 of day 4, will optimize soon

--- src/day4.py
+++ src/day4.py
@@ -12,11 +12,20 @@ for i in f:
             contains = False
     if not contains:
         continue
+    unencrypt = []
     for c in encrypt:
         if count.has_key(c):
             count[c] = count[c]+1
         else:
             count[c] = 1
+        if chr((ord(c) + (room%26))) < 'z':
+            unencrypt.append(chr(ord(c) + (room % 26)))
+        else:
+            unencrypt.append(chr(ord(c)-(26-(room%26))))
+    output = ''.join(unencrypt)
+    if output.find('northpole') != -1:
+        print output
+        print room
     for c in code:
         index = code.index(c)
         index = -index -1
