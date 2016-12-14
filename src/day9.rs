07646452fbaf2c887d301dc45ce23a12f3d2ca7d
Daniel Sauve <djsauve@rogers.com>
2016-12-14 10:39:41 -0500

    Prints off both numbers for day 9 part 1 and part2
    The function is basically the loop with an added check to decompress the markers within markers. Need to figure out why it's panicking

--- src/day9.rs
+++ src/day9.rs
@@ -37,6 +37,50 @@ fn main() {
         }
         input = input.index(end_index+1+num_chars..).to_string();
     }
-    println!("{}", decompressed);
     println!("{}", decompressed.len());
+    while decompressed.find("(").unwrap() > 0{
+        decompressed = decompress(decompressed);
+        println!("{}", decompressed.len());
+    }
+    println!("{}", decompressed.len());
+}
+
+fn decompress(input2 : String) -> String {
+    let mut input = input2;
+    let mut decompressed = String::new();
+    while input.len()>0{
+        let index = input.find("(");
+        match index {
+            Some(_) => (),
+            None => break
+        }
+        let index = index.unwrap();
+        let index2 : usize;
+        let end_index = input.find(")").unwrap();
+        let num_chars : usize;
+        {
+            let (first, second) = input.split_at(index);
+            for c in first.chars(){
+                decompressed.push(c);
+            }
+            index2 = second.find(")").unwrap();
+            let (decode, _) = second.split_at(index2);
+            let x_index = decode.find("x").unwrap();
+            num_chars = decode.index(1..x_index).parse().unwrap();
+            let num_repeat : usize = decode.index(x_index+1..).parse().unwrap();
+            let repeat_chars = input.index(end_index+1..end_index+num_chars+1);
+            if repeat_chars.contains("(") {
+                let repeat_chars = decompress(repeat_chars.to_string());
+                for _ in 0..num_repeat{
+                    decompressed.push_str(repeat_chars.as_str());
+                }
+            } else {
+                for _ in 0..num_repeat{
+                    decompressed.push_str(repeat_chars);
+                }
+            }
+        }
+        input = input.index(end_index+1+num_chars..).to_string();
+    }
+    decompressed
 }
