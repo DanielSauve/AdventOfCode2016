65fb70111a60e1c06a293b134af619243dcc295e
Daniel Sauve <djsauve@rogers.com>
2017-08-14 16:00:03 -0400

    Fixed some old crappy code

--- src/day9.rs
+++ src/day9.rs
@@ -9,34 +9,8 @@ fn main() {
         Err(e) => panic!("Could not open file: {}", e),
         Ok(_) => ()
     }
-    let mut decompressed = String::new();
-    while input.len()>0{
-        let index = input.find("(");
-        match index {
-            Some(_) => (),
-            None => break
-        }
-        let index = index.unwrap();
-        let index2 : usize;
-        let end_index = input.find(")").unwrap();
-        let num_chars : usize;
-        {
-            let (first, second) = input.split_at(index);
-            for c in first.chars(){
-                decompressed.push(c);
-            }
-            index2 = second.find(")").unwrap();
-            let (decode, _) = second.split_at(index2);
-            let x_index = decode.find("x").unwrap();
-            num_chars = decode.index(1..x_index).parse().unwrap();
-            let num_repeat : usize = decode.index(x_index+1..).parse().unwrap();
-            let repeat_chars = input.index(end_index+1..end_index+num_chars+1);
-            for _ in 0..num_repeat{
-                decompressed.push_str(repeat_chars);
-            }
-        }
-        input = input.index(end_index+1+num_chars..).to_string();
-    }
+    println!("{}", input.len());
+    let mut decompressed = decompress(input);
     println!("{}", decompressed.len());
     while decompressed.find("(").unwrap() > 0{
         decompressed = decompress(decompressed);
